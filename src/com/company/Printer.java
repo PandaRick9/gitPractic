da2186c3b05ef70b8ea501dff09dd2b96ecf04d1
Danik <herettyr765@gmail.com>
2022-02-19 23:55:50 +0300

    createt test

--- src/com/company/Printer.java
+++ src/com/company/Printer.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class Printer {
     public static void main(String[] args) {
-        System.out.println("I'm printer");
+        System.out.println("I'm printer Lolo");
     }
 }
